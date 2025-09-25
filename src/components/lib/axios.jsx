83fdd69422838542d4f46da13d8ed0ee256a9060
BAA77 <142021188+BAA77@users.noreply.github.com>
2025-09-25 15:24:23 -0700

    api

--- src/components/lib/axios.jsx
+++ src/components/lib/axios.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://socio-tech-server.onrender.com",
+    baseURL: "https://socio-tech-server.onrender.com/api",
     withCredentials: true
 });
 
